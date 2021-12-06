9b60764b67ec0bc71f744b8003cdf14287fd08f3
JousefAly <youssefali00286@gmail.com>
2021-12-06 20:08:31 +0200

    Added ProductDetails

--- ITech/Data/Entites/Product.cs
+++ ITech/Data/Entites/Product.cs
@@ -6,21 +6,22 @@ using System.Threading.Tasks;
 
 namespace ITech.Data.Entites
 {
-    
-        public class Product
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string ShortDescription { get; set; }
-            public string LongDescription { get; set; }
-            public decimal Price { get; set; }
-            public string Image1Url { get; set; }
-            public string Image2Url { get; set; }
-            public string Image3Url { get; set; }
-            public bool InStock { get; set; }
-            public int SoldCount { get; set; }
-            public Category Category { get; set; }
 
-        }
-    
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public string Image1Url { get; set; }
+        public string Image2Url { get; set; }
+        public string Image3Url { get; set; }
+        public bool InStock { get; set; }
+        public int SoldCount { get; set; }
+        public Category Category { get; set; }
+        public List<ProductDetail> ProductDetails { get; set; }
+
+
+    }
+
 }
