f4e6c920d410b4c1273e1619c60afa8b873c599c
JousefAly <youssefali00286@gmail.com>
2021-12-07 03:20:46 +0200

    Removed Update Names action

--- ITech/Controllers/HomeController.cs
+++ ITech/Controllers/HomeController.cs
@@ -38,53 +38,6 @@ namespace ITech.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult UpdateNames()
-        {
-            var products = _productRepository.GetAllProducts();
-            int affectedRows = 0;
-            for (int i = 1; i <= products.Count; i++)
-            {
-                if (i == 1 || i == 6)
-                {
-
-
-                    products[i - 1].Image1Url = "/img/mockImages/Mock1.1";
-                    products[i - 1].Image2Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image3Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image4Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image5Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image6Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image7Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image8Url = "/img/mockImages/EmptyMock";
-                }
-                else if (i > 5)
-                {
-                    products[i - 1].Image1Url = "/img/mockImages/Mock" + (i - 5) + ".1";
-                    products[i - 1].Image2Url = "/img/mockImages/Mock" + (i - 5) + ".2";
-                    products[i - 1].Image3Url = "/img/mockImages/Mock" + (i - 5) + ".3";
-                    products[i - 1].Image4Url = "/img/mockImages/Mock" + (i - 5) + ".4";
-                    products[i - 1].Image5Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image6Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image7Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image8Url = "/img/mockImages/EmptyMock";
-                }
-                else
-                {
-                    products[i - 1].Image1Url = "/img/mockImages/Mock" + i + ".1";
-                    products[i - 1].Image2Url = "/img/mockImages/Mock" + i + ".2";
-                    products[i - 1].Image3Url = "/img/mockImages/Mock" + i + ".3";
-                    products[i - 1].Image4Url = "/img/mockImages/Mock" + i + ".4";
-                    products[i - 1].Image5Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image6Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image7Url = "/img/mockImages/EmptyMock";
-                    products[i - 1].Image8Url = "/img/mockImages/EmptyMock";
-                }
-
-                _productRepository.SaveChanges();
-                affectedRows += 1;
-            }
-            ViewBag.AffectedRows = affectedRows;
-            return View();
-        }
+      
     }
 }
