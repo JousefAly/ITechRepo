440a01c446bfea2df85d0d7554ebdae6e4c1225d
JousefAly <youssefali00286@gmail.com>
2021-12-14 04:24:07 +0200

    Updated startup with new Identity AppUser

---
+++ ITech/Data/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITech.Data
+{
+    public class AppUser : IdentityUser
+    {
+        //IdentityUser already has Id prop
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
