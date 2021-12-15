ce73af040d18ac2a38f317cae18c994829b10105
JousefAly <youssefali00286@gmail.com>
2021-12-15 22:19:34 +0200

    Added Profile picture column

--- ITech/Data/AppUser.cs
+++ ITech/Data/AppUser.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ITech.Data
 {
@@ -12,5 +7,6 @@ namespace ITech.Data
         //IdentityUser already has Id prop
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public byte[] ProfilePicture { get; set; }
     }
 }
