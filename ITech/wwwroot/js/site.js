0f9ab99b14565119ed110a1872bd8386bad0fd6f
Mohamed harun <mohamedharon221@gmail.com>
2021-12-16 16:40:09 +0200

    Add new sidebar

--- ITech/wwwroot/js/site.js
+++ ITech/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//function Display() {
+//    var x = document.getElementById("side_content");
+//    if (x.style.visibility === "hidden") {
+//        x.style.visibility = "visible";
+//    } else {
+//        x.style.visibility = "hidden";
+//    }
+       
+//}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main2").style.marginLeft = "40px";
+    document.getElementById("main3").style.marginLeft = "100px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main2").style.marginLeft = "0";
+    document.getElementById("main3").style.marginLeft= "0";
+}
