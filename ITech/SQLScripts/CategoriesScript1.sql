f7e520c99913c49afe6aade32504af6caf9a2b1a
JousefAly <youssefali00286@gmail.com>
2022-01-01 23:28:31 +0200

    seed products and categories migrations

---
+++ ITech/SQLScripts/CategoriesScript1.sql
@@ -0,0 +1,5 @@
+﻿
+SET IDENTITY_INSERT dbo.Categories ON
+INSERT INTO dbo.Categories(Id, Name, Description) VALUES (1,'Phones','This category sells phones');
+INSERT INTO dbo.Categories(Id, Name, Description) VALUES (2,'Laptops','This category sells Laptops');
+SET IDENTITY_INSERT dbo.Categories OFF
