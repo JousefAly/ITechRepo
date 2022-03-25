7d9a09c00980c484fba45167cf9d9d4b0c824699
JousefAly <youssefali00286@gmail.com>
2022-03-25 17:27:34 +0200

    added actions with views for edit

---
+++ ITech/Controllers/EditProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITech.Controllers
+{
+    public class EditProductController : Controller
+    {
+        public IActionResult EditMainInformation(int productId)
+        {
+            return View();
+        }
+        public IActionResult EditDetails(int productId)
+        {
+            return View();
+        }
+        public IActionResult EditImages(int productId)
+        {
+            return View();
+        }
+    }
+}
