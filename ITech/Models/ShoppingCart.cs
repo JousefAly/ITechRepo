dbad406527038501a36144a9e89132fc0957530f
JousefAly <youssefali00286@gmail.com>
2022-04-01 18:05:50 +0200

    Implemented ShoppingCart functions

--- ITech/Models/ShoppingCart.cs
+++ ITech/Models/ShoppingCart.cs
@@ -1,5 +1,7 @@
 ﻿using ITech.Data;
+using ITech.Data.Entites;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -29,5 +31,67 @@ namespace ITech.Models
             session.SetString("CartId", cartId);
             return new ShoppingCart(context) { ShoppingCartId = cartId };
         }
+        // return the shopping cart item with it is new state
+        public ShoppingCartItem AddToCart(Product product, int amount)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems
+                .FirstOrDefault(i => i.Product.Id == product.Id && i.ShoppingCartId == ShoppingCartId);
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Product = product,
+                    Amount = amount,
+                };
+                _context.Add(shoppingCartItem);
+            }
+            else
+            {
+
+                shoppingCartItem.Amount += amount;
+            }
+            _context.SaveChanges();
+            return shoppingCartItem;
+        }
+        //return the remaining amount of a shopping cart item
+        public int RemoveFromCart(Product product, int amount = 1)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems
+                 .FirstOrDefault(i => i.Product.Id == product.Id && i.ShoppingCartId == ShoppingCartId);
+            if (shoppingCartItem == null)
+                return 0;
+            if (shoppingCartItem.Amount <= 1)
+            {
+                _context.Remove(shoppingCartItem);
+                _context.SaveChanges();
+                return 0;
+            }
+            shoppingCartItem.Amount -= amount;
+            _context.SaveChanges();
+            return shoppingCartItem.Amount;
+        }
+        
+        public bool ClearCart()
+        {
+            var shoppingCartItems = _context.ShoppingCartItems
+                .Where(i => i.ShoppingCartId == ShoppingCartId).ToList();
+
+            _context.RemoveRange(shoppingCartItems);
+            return _context.SaveChanges() > 0;
+        }
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return _context.ShoppingCartItems
+                .Where(i => i.ShoppingCartId == ShoppingCartId)
+                .Include(i => i.Product).ToList();
+        }
+        public decimal GetShoppingCartTotal()
+        {
+            return _context.ShoppingCartItems
+                .Where(i => i.ShoppingCartId == ShoppingCartId)
+                .Select(i => i.Amount * i.Product.PriceAfterDiscount).Sum();
+        }
+
     }
 }
