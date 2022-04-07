67cb86c75cbacc5989a75d216d18b797c130913f
JousefAly <youssefali00286@gmail.com>
2022-04-08 00:37:26 +0200

    Added Check Notification

--- ITech/Data/Entites/Notification.cs
+++ ITech/Data/Entites/Notification.cs
@@ -13,9 +13,22 @@ namespace ITech.Data.Entites
             Id = Guid.NewGuid().ToString();
             SentTime = DateTime.Now;
         }
+        private bool _checked = false;
+        private DateTime _lastCheckTime = new DateTime();
         public string Id { get; set; }
         public string Message { get; set; }
         public DateTime SentTime { get; set; }
+        public bool Checked
+        {
+            get { return _checked; }
+            set
+            {
+                _checked = value;
+                if (value)
+                    _lastCheckTime = DateTime.Now;               
+            }
+        }
+        public DateTime LastCheckTime { get; set; }
         public string ReceiverId { get; set; }
         public AppUser Receiver { get; set; }
         public string SenderId { get; set; }
