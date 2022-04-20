e855d296010c11ddd96f4450a48657c9c4d6cbad
JousefAly <youssefali00286@gmail.com>
2022-04-20 06:29:34 +0200

    Added SavedProductsSummary View Component

---
+++ ITech/ViewComponents/SavedProductsSummary.cs
@@ -0,0 +1,24 @@
+﻿using ITech.Data;
+using ITech.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITech.ViewComponents
+{
+    public class SavedProductsSummary : ViewComponent
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly UserManager<AppUser> _userManager;
+
+        public SavedProductsSummary(IProductRepository productRepository,
+                                    UserManager<AppUser> userManager)
+        {
+            _productRepository = productRepository;
+            _userManager = userManager;
+        }
+        public IViewComponentResult Invoke()
+        {
+             return View(_productRepository.GetUserSavedProducts(_userManager.GetUserId(HttpContext.User)).Length);
+        }
+    }
+}
