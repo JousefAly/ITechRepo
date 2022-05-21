b7457def0e3f2970b419a3bf8d0e52646b885fed
JousefAly <youssefali00286@gmail.com>
2022-05-21 09:34:30 +0200

    Added ProductScripts

---
+++ ITech/Migrations/20220521073123_SeedProductImagesScript.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace ITech.Migrations
+{
+    public partial class SeedProductImagesScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlPath = Path.Combine("SQLScripts/Products/dbo.ProductImages.Table.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlPath));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete FROM dbo.ProductImages WHERE Id > 500 AND Id < 1728;");
+        }
+    }
+}
