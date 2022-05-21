7f781cd521a0a72b21e3cc87d91cf71d96e4ab79
JousefAly <youssefali00286@gmail.com>
2022-05-21 10:53:56 +0200

    Seeded Product With Related Data Successfully

---
+++ ITech/Migrations/20220521085030_SeedProductDetails.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace ITech.Migrations
+{
+    public partial class SeedProductDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlPath = Path.Combine("SQLScripts/Products/dbo.ProductDetails.Table.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlPath));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete FROM dbo.ProductDetails WHERE Id > 500 AND Id < 907;");
+        }
+    }
+}
